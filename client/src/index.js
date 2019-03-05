a68e1e01b6086c02294c62c3bd62b9023d7be8f5
Ivan <ivanmhale@gmail.com>
2019-03-05 17:03:25 -0500

    clean slate for client using CRA2

--- client/src/index.js
+++ client/src/index.js
@@ -1,54 +1,12 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import Provider from "./context/Provider";
-import Context from "./context/Context";
+ReactDOM.render(<App />, document.getElementById('root'));
 
-import Header from "./components/Header";
-import Modal from "./components/Modal";
-import Search from "./components/Search";
-import Profile from "./components/Profile";
-import SnackbarCompopnent from "./components/Snackbar";
-
-class App extends Component {
-  componentDidMount() {
-    fetch("/auth/user")
-      .then(res => res.json())
-      .then(parsed => this.setState({ user: parsed }));
-  }
-
-  state = {
-    user: {}
-  };
-
-  renderBody() {
-    switch (window.location.pathname) {
-      case "/profile":
-        return <Profile user={this.state.user} />;
-      default:
-        return <Search />;
-    }
-  }
-
-  render() {
-    return (
-      <Provider>
-        <Context.Consumer>
-          {context => {
-            return (
-              <div className="app">
-                <Header user={this.state.user} />
-                {this.renderBody()}
-                <Modal user={this.state.user} />
-                <SnackbarCompopnent />
-              </div>
-            );
-          }}
-        </Context.Consumer>
-      </Provider>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.querySelector("#root"));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
